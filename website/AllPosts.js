71c3406c61027992a85884959f03206f7bbb0081
Daniel Olivieri <dfolivieri@gmail.com>
2019-05-06 21:21:43 -0400

    Update AllPosts.js

--- website/AllPosts.js
+++ website/AllPosts.js
@@ -1,90 +1,115 @@
 
 import React from 'react';
 import OurBox from "./OurBox.js";
+import ListItems from "./ListItems.js";
 
-
+// load fire base
 import * as firebase from 'firebase'
 
-
-// set up firebase
+// set up firebase with keys
 function setup() {
-  var config = {
-    apiKey: "AIzaSyCNWTiYkNyqgSIR0bs5z9LsL3LB67h4MCE",
-    authDomain: "stalgia-167017.firebaseapp.com",
-    databaseURL: "https://stalgia-167017.firebaseio.com",
-    projectId: "stalgia-167017",
-    storageBucket: "stalgia-167017.appspot.com",
-    messagingSenderId: "586804764653"
-  };
-
-  firebase.initializeApp(config);
+    var config = {
+      apiKey: "AIzaSyCNWTiYkNyqgSIR0bs5z9LsL3LB67h4MCE",
+      authDomain: "stalgia-167017.firebaseapp.com",
+      databaseURL: "https://stalgia-167017.firebaseio.com",
+      projectId: "stalgia-167017",
+      storageBucket: "stalgia-167017.appspot.com",
+      messagingSenderId: "586804764653"
+    };
   
-}
-
-setup(); // sets up the firebase
-
-var database = firebase.database();
-
-var memories = database.ref();
-
-console.log(memories);
-
-var listItems;
-
-var textsToPost = [];
-
-
-// triggers get data function
-function queryData(data) {
- 
-memories.on("value", gotData, errData);
-}
-
-// error message
-function errData(error) {
-  console.log("Something went wrong.");
-  console.log(error);
-}
-
-
-// retrieve data 
-function gotData(data) {
-  console.log("inside gotData");
-  var memories = data.val();
-  // Grab the keys to iterate over the object
-  var keys = Object.keys(memories);
-
-  for (var i = 0; i < keys.length; i++) {
-    console.log("looping");
-    var key = keys[i];
-    // Look at each fruit object!
-    var memory = memories[key];    
-
-    if (memory.approved === 1) {
-      textsToPost.push(memory.text);
-    } 
-    
+      // initialize it 
+    firebase.initializeApp(config);
     
   }
-  console.log("mapping");
-  listItems = textsToPost.map((post) =>
-  <OurBox text={post}> </OurBox>
-);
+  
+  // sets up the firebase
+  setup(); 
 
-
-}
- 
 
 class AllPosts extends React.Component {
-  componentDidMount () {
-    queryData();
-    console.log("allPosts will mount");
-    this.forceUpdate();
+
+  constructor() {
+    super();
+    this.state = {
+      // place holder values 
+      items: ["first", "second", "third"],
+      listToPost: ["Loading"],
+
+    }
   }
+
+  // early in the React DOM life cycle
+  componentDidMount() {
+
+
+    // gets database
+    const database = firebase.database();
+    // putting it into new database
+    var memories = database.ref();
+
+    console.log(memories);
+
+    // get the data
+    memories.on('value', (snapshot) => {
+
+      // holds current snapshot of data 
+        let items = snapshot.val();
+        // creates newState array
+        let newState = [];
+        // for loop
+        for (let item in items) {
+
+          var isApproved = items[item].approved;
+
+          console.log(isApproved);
+
+          if (isApproved === 1){
+            newState.push({
+              id: item,
+              postText: items[item].text,
+          });
+          }
+            
+
+        }
+      
+        // sets the state
+        this.setState({
+            items: newState,        
+        });
+
+        console.log(this.state);
+
+        // array to holds only the texts
+        var textsToPost = [];
+
+        for (var i = 0; i < this.state.items.length; i++) {
+
+          console.log(this.state.items[i].postText);
+
+            textsToPost[i] = this.state.items[i].postText;
+
+        }
+
+        this.setState({
+          listToPost: textsToPost,        
+      });
+
+        console.log(this.state.listToPost);
+
+
+    })
+
+
+}
 
   render(){
 
-    return <div>{listItems}</div>
+    return <div> <ListItems list={this.state.listToPost} ></ListItems>
+
+
+</div>
+
 
   }
 }
